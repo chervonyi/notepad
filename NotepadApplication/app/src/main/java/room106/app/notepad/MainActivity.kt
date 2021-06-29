3407b9dfab912da86b751ce037b0ae521e67e5cd
chervonyi <yuri.ternopil@gmail.com>
2021-06-29 11:36:53 -0500

    Create a project

---
+++ NotepadApplication/app/src/main/java/room106/app/notepad/MainActivity.kt
@@ -0,0 +1,11 @@
+package room106.app.notepad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
